bb50a32b25a39c50cbc42e4ed9957c135f87bd8d
Valeriy Kudryk <58161114+wakud@users.noreply.github.com>
2022-04-29 22:32:10 +0300

    https://github.com/wakud/PlayStationClub-develop/projects/1#card-81307992

---
+++ PlayStationClub/Pages/Contact/Contacts.cshtml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using PlayStationClub.Data;
+
+
+namespace PlayStationClub.Pages.Contacts
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
